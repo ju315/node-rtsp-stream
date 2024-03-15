5c35ec96a58e22fea8a4e6898928de25c2a0e43f
ju315 <pbju92@gmail.com>
2024-03-15 15:01:14 +0900

    fix: stream 데이터 중단 감지 로직 수정 및 ffmpeg 실행 커멘드 수정

--- mpeg1muxer.js
+++ mpeg1muxer.js
@@ -6,6 +6,7 @@ const Mpeg1Muxer = function(options) {
   this.url = options.url;
   this.useTcp = options.useTcp;
   this.ffmpegOptions = options.ffmpegOptions;
+  this.name = options.name;
   this.exitCode = undefined;
   this.additionalFlags = [];
 
@@ -18,15 +19,22 @@ const Mpeg1Muxer = function(options) {
     }
   }
 
+  // TOPView 실행 옵션: [rtsp_transport=tcp, allowed_media_types=video, max_delay=500000, stimeout=5000000]
+  // 해당 옵션은 Stream 시작할때 ffmpegOptions 값으로 전달.
+
   this.spawnOptions = [
     this.url,
     '-b:v', '1000k',
-    '-maxrate', '1000k',
-    '-bufsize', '1000k',
+
+    // '-maxrate', '1000k',
+    // '-bufsize', '1000k',
+
     // max bitrate possibly too small or try trellis with large lmax or increase qmax 경고 메세지로 인해 옵션 추가.
-    '-qmin', '16',
-    '-qmax', '51',
+    // '-qmin', '16',
+    // '-qmax', '51',
+
     '-an',
+
     // additional ffmpeg options go here
     ...this.additionalFlags,
     '-'
@@ -44,6 +52,7 @@ const Mpeg1Muxer = function(options) {
   });
 
   this.inputStreamStarted = true;
+  global.Logger.debug(`node-rtsp-stream(${this.name}):: ffmpeg stream start command ${JSON.stringify(this.spawnOptions)}`);
 
   this.stream.stdout.on('data', (data) => {
     return this.emit('mpeg1data', data);
@@ -54,9 +63,9 @@ const Mpeg1Muxer = function(options) {
   });
 
   this.stream.on('exit', (code, signal) => {
-    global.Logger.info(`node-rtsp-stream:: ffmpeg Stream on Exit Code. (code: ${code}, signal: ${signal})`);
+    global.Logger.error(`node-rtsp-stream(${this.name}):: ffmpeg Stream on Exit Code. (code: ${code}, signal: ${signal})`);
     if (code === 1) {
-      global.Logger.error('node-rtsp-stream:: RTSP stream exited with error')
+      global.Logger.error(`node-rtsp-stream(${this.name}):: RTSP stream exited with error`)
       this.exitCode = 1
       return this.emit('exitWithError');
     }
@@ -74,7 +83,7 @@ Mpeg1Muxer.prototype.close = function() {
   this.stream.kill();
   delete this.stream;
   this.inputStreamStarted = false;
-  global.Logger.info('node-rtsp-stream:: RTSP stream has stopped and destroyed.');
+  global.Logger.info(`node-rtsp-stream(${this.name}):: ffmpeg RTSP stream has stopped and destroyed.`);
 }
 
 util.inherits(Mpeg1Muxer, events.EventEmitter)
--- videoStream.js
+++ videoStream.js
@@ -18,7 +18,7 @@ const VideoStream = function(options) {
   this.ffmpegTimer = null;
   this.maxTimeGap = options.maxTimeGap || 30000;
 
-  global.Logger.info(`node-rtsp-stream:: Init VideoStream. (name: ${this.name}, streamUrl: ${this.streamUrl}, ws: ${this.wsHost}:${this.wsPort})`);
+  global.Logger.info(`node-rtsp-stream(${this.name}):: Init VideoStream. (name: ${this.name}, streamUrl: ${this.streamUrl}, ws: ${this.wsHost}:${this.wsPort})`);
   this.startMpeg1Stream();
   this.pipeStreamToSocketServer();
   return this
@@ -27,7 +27,7 @@ const VideoStream = function(options) {
 util.inherits(VideoStream, events.EventEmitter)
 
 VideoStream.prototype.stop = function() {
-  global.Logger.error(`node-rtsp-stream:: Stop VideoStream. (name: ${this.name}, streamUrl: ${this.streamUrl}, ws: ${this.wsHost}:${this.wsPort})`);
+  global.Logger.error(`node-rtsp-stream(${this.name}):: Stop VideoStream. (name: ${this.name}, streamUrl: ${this.streamUrl}, ws: ${this.wsHost}:${this.wsPort})`);
   if (this.wsClient.length) {
     for (const client of this.wsClient) {
       client.send('socket will be closed');
@@ -42,7 +42,7 @@ VideoStream.prototype.stop = function() {
 }
 
 VideoStream.prototype.restart = function() {
-  global.Logger.info(`node-rtsp-stream:: Restart VideoStream. (name: ${this.name}, streamUrl: ${this.streamUrl}, ws: ${this.wsHost}:${this.wsPort})`);
+  global.Logger.info(`node-rtsp-stream(${this.name}):: Restart VideoStream. (name: ${this.name}, streamUrl: ${this.streamUrl}, ws: ${this.wsHost}:${this.wsPort})`);
   this.startMpeg1Stream();
 }
 
@@ -58,6 +58,7 @@ VideoStream.prototype.startMpeg1Stream = function() {
     url: this.streamUrl,
     ffmpegPath: this.options.ffmpegPath == undefined ? "ffmpeg" : this.options.ffmpegPath,
     useTcp: this.options.useTcp,
+    name: this.name,
   });
 
   this.stream = this.mpeg1Muxer.stream;
@@ -81,9 +82,6 @@ VideoStream.prototype.startMpeg1Stream = function() {
   outputData = [];
 
   this.mpeg1Muxer.on('ffmpegStderr', (data) => {
-    const nowDate = new Date().getTime();
-    lastUtc = nowDate;
-
     let size;
     data = data.toString();
 
@@ -116,11 +114,21 @@ VideoStream.prototype.startMpeg1Stream = function() {
   });
 
   this.mpeg1Muxer.on('ffmpegStderr', function(data) {
-    return global.process.stderr.write(data);
+    const msg = data.toString();
+
+    if (msg.includes('frame=')) {
+      process.env.NODE_ENV && global.process.stderr.write(data);
+      return;
+    }
+
+    process.env.NODE_ENV && global.process.stderr.write(data);
+    global.Logger.debug(`node-rtsp-stream(${this.name}):: ${msg}`)
+    return;
   });
 
   this.mpeg1Muxer.on('exitWithError', () => {
-    global.Logger.error(`node-rtsp-stream:: ${this.name} ffmpeg get exitWithError.`);
+    global.Logger.error(`node-rtsp-stream(${this.name}):: ${this.name} ffmpeg get exitWithError.`);
+    clearInterval(this.ffmpegTimer);
 
     return this.emit('exitWithError');
   });
@@ -130,7 +138,7 @@ VideoStream.prototype.startMpeg1Stream = function() {
 
     if (nowUtc < lastUtc + this.maxTimeGap) return;
 
-    global.Logger.error(`node-rtsp-stream:: The stream is terminated because no stream data has been received for a certain period of time. (name: ${this.name}, ms: ${this.maxTimeGap})`);
+    global.Logger.error(`node-rtsp-stream(${this.name}):: The stream is terminated because no stream data has been received for a certain period of time. (name: ${this.name}, ms: ${this.maxTimeGap})`);
 
     clearInterval(this.ffmpegTimer);
     this.stop();
@@ -157,7 +165,7 @@ VideoStream.prototype.pipeStreamToSocketServer = function() {
         if (client.readyState === 1) {
           results.push(client.send(data, opts));
         } else {
-          global.Logger.error(`node-rtsp-stream:: Error. Client from remoteAddress ${client.remoteAddress} not connected.`);
+          global.Logger.error(`node-rtsp-stream(${this.name}):: Error. Client from remoteAddress ${client.remoteAddress} not connected.`);
           results.push(console.log("Error: Client from remoteAddress " + client.remoteAddress + " not connected."))
         }
       }
@@ -168,7 +176,7 @@ VideoStream.prototype.pipeStreamToSocketServer = function() {
       return this.wsServer.broadcast(data)
     });
   } catch (err) {
-    global.Logger.error(`node-rtsp-stream:: ${this.name} broadcast Socket something went wrong.(err: ${err.message})`);
+    global.Logger.error(`node-rtsp-stream(${this.name}):: ${this.name} broadcast Socket something went wrong.(err: ${err.message})`);
     console.log('something went wrong...');
     console.log(err);
   }
@@ -186,13 +194,13 @@ VideoStream.prototype.onSocketConnect = function(socket, request) {
   });
 
   const clientIp = request.headers["x-forwarded-for"] || request.connection.remoteAddress;
-  global.Logger.info(`node-rtsp-stream:: ${this.name}: New WebSocket Connection. (clientIp: ${clientIp}, total: ${this.wsServer.clients.size})`);
+  global.Logger.info(`node-rtsp-stream(${this.name}):: ${this.name}: New WebSocket Connection. (clientIp: ${clientIp}, total: ${this.wsServer.clients.size})`);
 
   socket.remoteAddress = request.connection.remoteAddress;
 
   return socket.on("close", (code, message) => {
-    global.Logger.info(`node-rtsp-stream:: ${this.name}: Disconnected WebSocket. (clientIp: ${clientIp}, code: ${code}, message: ${message || null}, total: ${this.wsServer.clients.size})`);
-    return console.log(`${this.name}: Disconnected WebSocket (${this.wsServer.clients.size} total)`);
+    global.Logger.info(`node-rtsp-stream(${this.name}):: ${this.name}: Disconnected WebSocket. (clientIp: ${clientIp}, code: ${code}, message: ${message || null}, total: ${this.wsServer.clients.size})`);
+    return;
   })
 }
 
